f4ca74dc888f2551efabcf88407bf607ba8db874
viktor-elovikov <viktor_elovikov@mail.ru>
2017-04-06 10:57:00 +0500

    working betta version

--- main.cpp
+++ main.cpp
@@ -1,75 +1,78 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+
 using namespace std;
-const int inf=16000;
+const int inf = 16000;
 
-void full(int a[100][100],int n)
+void full(int mas[100][100],int amount_vertex)
 {
-    int c,e,f;
-    scanf("%d %d %d",&c,&e,&f);
-    for (int i=0; i<n; ++i)
-        for (int j=0; j<n; ++j)
+    int first_vertex,second_vertex,Rib_weight;
+    //Primary zeroing of the matrix
+    for (int i = 0; i < amount_vertex; ++i)
+        for (int j = 0; j < amount_vertex; ++j)
         {
-            if (i==j) a[i][j] = 0;
-            else a[i][j] = inf;
+            if (i == j) mas[i][j] = 0;
+            else mas[i][j] = inf;
         }
-    while (c != -1)
+    //Filling the matrix where i,j element is the weight of the edge
+    printf("please enter first_vertex second_vertex Rib weight ");
+    scanf("%d %d %d",&first_vertex,&second_vertex,&Rib_weight);
+    while (first_vertex != -1)
     {
-        a[c-1][e-1]=f;
-        scanf("%d %d %d",&c,&e,&f);
+        mas[first_vertex - 1][second_vertex - 1] = Rib_weight;
+        scanf("%d %d %d",&first_vertex,&second_vertex,&Rib_weight);
     }
-    c=0;
-    for (int i=0; i<n; ++i)
+    //
+    first_vertex = 0;
+    for (int i = 0; i < amount_vertex; ++i)
     {
-
-        for (int j=1+c; j<n; ++j)
-            a[j][i]=a[i][j];
-        c++;
+        for (int j = 1 + first_vertex; j < amount_vertex; ++j)
+            mas[j][i] = mas[i][j];
+        first_vertex++;
     }
 }
 
 int main()
 {
-    int n,i,j,k,a[100][100];
-    scanf("%d", &n);
-    full(a,n);
+    int amount_vertex,mas[100][100];
+    scanf("%d", &amount_vertex);
+    full(mas,amount_vertex);
 
-    for (i=0; i<n; ++i)
+    for (int i = 0; i < amount_vertex; ++i)
     {
-        for (j=0; j<n; ++j)
-            printf("%5d ",a[i][j]);
+        for (int j = 0; j < amount_vertex; ++j)
+            printf("%5d ",mas[i][j]);
         printf("\n");
     }
 
-    for (k=0;k<n;++k)
-        for (i=0;i<n;++i)
-            for (j=0;j<n;++j)
-                if (a[i][k]<inf && a[k][j]<inf) a[i][j]=min(a[i][j],a[i][k]+a[k][j]);
+    for (int k = 0; k < amount_vertex; ++k)
+        for (int i = 0; i < amount_vertex; ++i)
+            for (int j = 0; j < amount_vertex; ++j)
+                if (mas[i][k] < inf && mas[k][j] < inf) mas[i][j] = min(mas[i][j],mas[i][k] + mas[k][j]);
 
     printf ("\n");
 
-    for (i=0; i<n; ++i)
+    for (int i = 0; i < amount_vertex; ++i)
     {
-        for (j=0; j<n; ++j)
-            printf("%5d ",a[i][j]);
+        for (int j = 0; j < amount_vertex; ++j)
+            printf("%5d ",mas[i][j]);
         printf("\n");
     }
-
-    int sum=16000, sum1, med_v;
-    for (i=0; i<n; ++i)
+    //Finding the median of the graph
+    int sum = 16000, sum1, med_v;
+    for (int i = 0; i < amount_vertex; ++i)
     {
-        sum1=0;
-        for(j=0; j<n; ++j)
-            if (a[i][j]<16000) sum1 += a[i][j];
+        sum1 = 0;
+        for(int j = 0; j < amount_vertex; ++j)
+            if (mas[i][j] < 16000) sum1 += mas[i][j];
         if (sum > sum1)
             {
-                sum=sum1;
-                med_v=i;
+                sum = sum1;
+                med_v = i;
             }
     }
-    printf("%d \n",sum);
     printf("%d ",med_v);
-system("pause");
-return 0;
+    system("pause");
+    return 0;
 }
